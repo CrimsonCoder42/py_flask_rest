5ce3f446be8c0f5876b4f25e741b40d13fae6184
CrimsonCoder42 <nostro37@gmail.com>
2023-01-31 10:58:42 -0500

    basics done

---
+++ refresher/strings.py
@@ -0,0 +1,24 @@
+name = "Bob"
+
+
+# greeting = f"Hello, {name}"
+
+# print(greeting)
+#
+# name = "Rolf"
+#
+# print(greeting)
+
+# greeting = "Hello, {}"
+#
+# with_name = greeting.format(name)
+# with_name_two = greeting.format("Rolf")
+#
+# print(with_name)
+# print(with_name_two)
+
+
+
+
+
+
---
+++ refresher/user_input.py
@@ -0,0 +1,10 @@
+# name = input("What is your name? ")
+# print(name)
+
+
+size_input = input("How big is your house (in square feet): ")
+square_feet = int(size_input)
+
+square_meters = round(square_feet / 10.8)
+
+print(square_meters)
