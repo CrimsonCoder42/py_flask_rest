0a918b893108b1b9edce5ce53698701abd5ff0fe
CrimsonCoder42 <nostro37@gmail.com>
2023-02-01 09:53:50 -0500

    lambda functions

--- refresher/return_values.py
+++ refresher/return_values.py
@@ -1,6 +1,19 @@
-def add(x, y):
-    return x + y
+# def add(x, y):
+#     return x + y
+#
+#
+# result = add(5, 8)
+# print(result)
 
 
-result = add(5, 8)
+def divide(dividend, divisor):
+    if divisor != 0:
+        return dividend / divisor
+    else:
+        return "You fool"
+
+
+result = divide(15, 0)
 print(result)
+
+
