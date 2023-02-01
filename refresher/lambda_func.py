17d4c68a02ec52e5bce949c838f8ade3e2d70ec6
CrimsonCoder42 <nostro37@gmail.com>
2023-02-01 10:49:23 -0500

    unpacking functions

--- refresher/lambda_func.py
+++ refresher/lambda_func.py
@@ -6,13 +6,18 @@
 
 # print(add(5, 7))
 
-def double(x):
-    return x * 2
+# def double(x):
+#     return x * 2
+#
+#
+# sequence = [1, 3, 5, 9]
+# # doubled = [double(x) for x in sequence]
+# doubled = map(double, sequence)
+#
+# print(doubled)
 
 
-sequence = [1, 3, 5, 9]
-# doubled = [double(x) for x in sequence]
-doubled = map(double, sequence)
 
-print(doubled)
+
+
 
