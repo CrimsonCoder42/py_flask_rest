88559798fc50bcd8efac36345e255997450f3856
CrimsonCoder42 <nostro37@gmail.com>
2023-01-31 21:16:15 -0500

    return value repeat

--- refresher/functions.py
+++ refresher/functions.py
@@ -15,5 +15,26 @@
 #
 # user_age_in_seconds()
 
+# def add(x, y):
+#     print(x + y)
+#
+# add(5, 3)
+
+# def say_hello(name, surname):
+#     print(f"hello, {name}")
+#
+#
+# say_hello("Devin", "Anderson")
+#
+
+#
+# def divide(dividend, divisor):
+#     if divisor != 0:
+#         print(dividend / divisor)
+#     else:
+#         print("You Fool")
+#
+#
+# divide(dividend=15, divisor=2)
 
 
